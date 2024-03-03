0e2f0624ccc60318993f0aea8f737c69d740554d
kitamuraDev <masato1999824@icloud.com>
2024-03-03 10:54:20 +0900

    :heavy_plus_sign: Add Tailwind CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {
-  title = 'saizeriya-gacha';
-}
+export class AppComponent {}
